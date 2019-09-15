1e9f196592ad344fc672698e93d339cb0f5609b5
Denis Verkhoturov <mod.satyr@gmail.com>
2019-09-15 16:37:45 +0300

    chore: Initialize the project

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const HelloWorld: React.FC = () => (<h1>Cromwell Pipeline</h1>);
+
+ReactDOM.render(<HelloWorld/>, document.getElementById('root'));
